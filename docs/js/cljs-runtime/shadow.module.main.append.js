accff70814d9980e6e27ac8920b80c32641b4e3b
sabingurung <sabingurung68@gmail.com>
2022-12-25 00:21:37 +0100

    deleted

--- docs/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { cash_split.core.init(); } catch (e) { console.error("An error occurred when calling (cash-split.core/init)"); throw(e); }
